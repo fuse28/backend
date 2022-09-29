77534c2263fd033a8e14fcb5402ae220c1dccdff
fuse28 <mixfuse92@gmail.com>
2022-09-29 22:52:46 +0530

    adding new knowledge

--- fruitProject/app.js
+++ fruitProject/app.js
@@ -4,28 +4,34 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB", {
   useNewUrlParser: true,
 });
 
-const personScheme = new mongoose.Schema({
-  name: String,
-  age: Number,
-});
+// const personScheme = new mongoose.Schema({
+//   name: String,
+//   age: Number,
+// });
 
-const Person = mongoose.model("Person", personScheme);
+// const Person = mongoose.model("Person", personScheme);
 
-const person = new Person({
-  name: "John",
-  age: 37,
-});
+// const person = new Person({
+//   name: "John",
+//   age: 37,
+// });
 
 // person.save();
 
-// const fruitsSchema = new mongoose.Schema({
-//   name: String,
-//   rating: Number, //blueprint of database
-//   review: String,
-// });
+const fruitsSchema = new mongoose.Schema({
+  name: String,
+  rating: Number, //blueprint of database
+  review: String,
+});
 
-// const Fruit = mongoose.model("Fruit", fruitsSchema);
+const Fruit = mongoose.model("Fruit", fruitsSchema);
 
+const berry = new Fruit({
+  name: "berry",
+  rating: 2,
+  review: "berries are toxic",
+});
+berry.save();
 // const fruit = new Fruit({
 //   name: "Apple",
 //   rating: 7,
@@ -64,15 +70,87 @@ const person = new Person({
 
 // fruit.save();
 
-//to find the data into databases
+//!adding validations
+// const fruitsSchema = new mongoose.Schema({
+//   //   name: {
+//   //     type: String,
+//   //     required: [true, "Name is required"],
+//   //   },
+//   name: String,
+//   rating: {
+//     type: Number,
+//     min: 1,
+//     max: 10,
+//   },
+//   review: String,
+// });
+// const Fruit = mongoose.model("Fruit", fruitsSchema);
 
-Fruit.find(function (err, fruits) {
-  if (err) {
-    console.log(err);
-  } else {
-    fruits.forEach(function (fruit) {
-      console.log(fruit.name);
-    });
-    mongoose.connection.close();
-  }
+// const fruit = new Fruit({
+//   rating: 4,
+//   review: "pineapples are tangi",
+// });
+
+// fruit.save();
+
+//to find the data into databases
+// Fruit.find(function (err, fruits) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     fruits.forEach(function (fruit) {
+//       console.log(fruit.name);
+//     });
+//     // mongoose.connection.close();
+//   }
+// });
+
+//? updating data
+
+// Fruit.updateOne(
+//   { _id: "6335cb6475de800bb76c53b4" },
+//   { name: "pineapple" },
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Updated successfully");
+//     }
+//     mongoose.connection.close();
+//   }
+// );
+
+//* deleting data
+// Fruit.deleteOne({ _id: "6335cabbd79e53a4c4a72d49" }, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("deleted successfully");
+//   }
+// });
+
+//*deleting multiple entries
+
+// Person.deleteMany({ name: "John" }, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("deleted from peoples data");
+//   }
+// });
+
+//! embedding doc and establishing relationships
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favouriteFruit: fruitsSchema,
 });
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "Eren",
+  age: 23,
+});
+
+person.save();
